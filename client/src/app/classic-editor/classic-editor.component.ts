071607e3314617da9bbb0244da3e92664132d029
Kvetkakiwi <kvetoslava.vojtechova@assecosol.com>
2023-08-23 15:19:55 +0200

    feat: implements simple classic ck5 editor

--- client/src/app/classic-editor/classic-editor.component.ts
+++ client/src/app/classic-editor/classic-editor.component.ts
@@ -1,17 +1,21 @@
 import { Component, NgModule } from '@angular/core';
 
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 @Component({
-  selector: 'app-classic-editor',
+  selector: 'classic-editor',
   templateUrl: './classic-editor.component.html',
   styleUrls: ['./classic-editor.component.scss']
 })
 export class ClassicEditorComponent {
-
+  public Editor = ClassicEditor;
+  data = "<p>Hello, world!</p>";
 }
 
 @NgModule({
-  declarations: [ClassicEditorComponent],
-  imports: [],
-  exports: [ClassicEditorComponent]
+  declarations: [ ClassicEditorComponent ],
+  imports: [ CKEditorModule ],
+  exports: [ ClassicEditorComponent ]
 })
 export class ClassicEditorModule {}
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,12 @@
+/// <reference types="@angular/localize" />
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import '@assecosolutions/fox-card';
+import '@assecosolutions/fox-text';
+import '@assecosolutions/fox-button';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
