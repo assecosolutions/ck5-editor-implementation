0089937341d959f50250e181753eb74a71c29644
Kvetkakiwi <kvetoslava.vojtechova@assecosol.com>
2023-08-29 10:53:48 +0200

    feat(client): [AS-1031][AS-3183] implements custom ck build in custom-editor component

--- client/src/app/custom-editor/custom-editor.component.ts
+++ client/src/app/custom-editor/custom-editor.component.ts
@@ -1,14 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import * as customBuild from '../../ck-custom-build/build/ckeditor';
 
 @Component({
-  selector: 'app-custom-editor',
+  selector: 'custom-editor',
   templateUrl: './custom-editor.component.html',
   styleUrls: ['./custom-editor.component.scss']
 })
 export class CustomEditorComponent {
-
+  public htmlData:string = "<p>Hello, world!</p>";
+  public Editor = customBuild as any;
 }
 
 @NgModule({
